91b8cbe578ca1189b83e657de07265c79e9e4d00
rrfsup <jc8464@me.com>
2021-06-14 10:01:20 -0400

    Changed save and load function names

--- js/formify.js
+++ js/formify.js
@@ -38,7 +38,7 @@ function escape(str) {
         .replace(/[\t]/g, '\\t');
 }
 
-function loadData() {
+function loadContractDetails() {
     let input = document.getElementById("loadFileInput");
     let file = input.files[0];
     let reader = new FileReader();
@@ -93,7 +93,7 @@ function loadData() {
     };
 }
 
-function saveData()
+function saveContractDetails()
 {
     let ticketDate = document.getElementById("invoiceDateInput");
     let ticketNumber = document.getElementById("invoiceNumberInput");
